6d226392f7694c67da43e95b18052e0cd553d63d
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2023-02-07 22:59:04 +0530

    Basic Socket.io

---
+++ basic.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const socketio = require('socket.io');
+
+const server = http.createServer((req, res) => {
+  res.end('I am connected');
+});
+
+// const io = socketio(server);
+// const io = socketio(server, {
+//   cors: {
+//     origin: '*',
+//   },
+// });
+
+const io = socketio(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['my-custom-header'],
+    credentials: false,
+  },
+});
+
+io.on('connection', (socket, req) => {
+  socket.emit('welcome', 'Welcome to the websocket server!!!');
+  socket.on('message', (msg) => {
+    console.log(msg);
+  });
+});
+
+server.listen(8000);
